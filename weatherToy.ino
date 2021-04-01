ce4be38a0761903a7c2be8ee518a49b96ad3b166
easean2 <easean2@icloud.com>
2021-04-01 16:10:24 +0800

    Update weatherToy.ino
    +增加天气显示界面
       缺接口获取天气

--- weatherToy.ino
+++ weatherToy.ino
@@ -2,6 +2,10 @@
 #include <U8g2lib.h>
 #include <SPI.h>
 #include <Wire.h>
+#include <ESP8266WiFi.h>          
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>      
 
 /*
  * 
@@ -14,17 +18,132 @@
  * 
 */
 
+#define SUN  0
+#define SUN_CLOUD  1
+#define CLOUD 2
+#define RAIN 3
+#define THUNDER 4
+
+
 //下面这行代码就是初始化针脚的，根据自己的接线设置clock和data即可，reset没有连接就用参数U8X8_PIN_NONE
 U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ 12, /* data=*/ 14, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the Display
 
+int16_t width = 0,height = 0;
+char buf[256];
+
+void drawWeatherSymbol(u8g2_uint_t x, u8g2_uint_t y, uint8_t symbol)
+{
+  // fonts used:
+  // u8g2_font_open_iconic_embedded_6x_t
+  // u8g2_font_open_iconic_weather_6x_t
+  // encoding values, see: https://github.com/olikraus/u8g2/wiki/fntgrpiconic
+  
+  switch(symbol)
+  {
+    case SUN:
+      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
+      u8g2.drawGlyph(x, y, 69);  
+      break;
+    case SUN_CLOUD:
+      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
+      u8g2.drawGlyph(x, y, 65); 
+      break;
+    case CLOUD:
+      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
+      u8g2.drawGlyph(x, y, 64); 
+      break;
+    case RAIN:
+      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
+      u8g2.drawGlyph(x, y, 67); 
+      break;
+    case THUNDER:
+      u8g2.setFont(u8g2_font_open_iconic_embedded_6x_t);
+      u8g2.drawGlyph(x, y, 67);
+      break;      
+  }
+}
+
+void drawWeather(uint8_t symbol, int degree)
+{
+  drawWeatherSymbol(0, 48, symbol);
+  u8g2.setFont(u8g2_font_logisoso32_tf);
+  u8g2.setCursor(48+3, 42);
+  u8g2.print(degree);
+  u8g2.print("°C");   // requires enableUTF8Print()
+}
+
+/*
+  Draw a string with specified pixel offset. 
+  The offset can be negative.
+  Limitation: The monochrome font with 8 pixel per glyph
+*/
+void drawScrollString(int16_t offset, const char *s)
+{
+  static char buf[36];  // should for screen with up to 256 pixel width 
+  size_t len;
+  size_t char_offset = 0;
+  u8g2_uint_t dx = 0;
+  size_t visible = 0;
+  len = strlen(s);
+  if ( offset < 0 )
+  {
+    char_offset = (-offset)/8;
+    dx = offset + char_offset*8;
+    if ( char_offset >= width/8 )
+      return;
+    visible = width/8-char_offset+1;
+    strncpy(buf, s, visible);
+    buf[visible] = '\0';
+    u8g2.setFont(u8g2_font_8x13_mf);
+    u8g2.drawStr(char_offset*8-dx, 62, buf);
+  }
+  else
+  {
+    char_offset = offset / 8;
+    if ( char_offset >= len )
+      return; // nothing visible
+    dx = offset - char_offset*8;
+    visible = len - char_offset;
+    if ( visible > width/8+1 )
+      visible = width/8+1;
+    strncpy(buf, s+char_offset, visible);
+    buf[visible] = '\0';
+    u8g2.setFont(u8g2_font_8x13_mf);
+    u8g2.drawStr(-dx, 62, buf);
+  }
+  
+}
+
+void draw(const char *s, uint8_t symbol, int degree)
+{
+  int16_t offset = -(int16_t)u8g2.getDisplayWidth();
+  int16_t len = strlen(s);
+  for(;;)
+  {
+    u8g2.firstPage();
+    do {
+      drawWeather(symbol, degree);
+      drawScrollString(offset, s);
+    } while ( u8g2.nextPage() );
+    delay(20);
+    offset+=2;
+    if ( offset > len*8+1 )
+      break;
+  }
+}
+
 void setup(void) {
   u8g2.begin();
+  u8g2.enableUTF8Print();
+  
+  width = u8g2.getDisplayWidth();
+  height = u8g2.getDisplayHeight();
+   
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("AutoConnectAP");
+  sprintf(buf, "%s%s","SSID:", WiFi.SSID().c_str());
 }
 
 void loop(void) {
-  u8g2.firstPage();
-  do {
-    u8g2.setFont(u8g2_font_ncenB14_tr);
-    u8g2.drawStr(0,24,"Hello World!");
-  } while ( u8g2.nextPage() );
+  draw(buf, CLOUD, 20);
 }
